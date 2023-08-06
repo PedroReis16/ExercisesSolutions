74dbebbcb7f6e9a478fd5aebce2c57072e7e84ec
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2023-08-06 17:45:51 -0300

    Problema 1000 beecrowd

---
+++ BeeCrowd/Problema 1000/Problema 1000 - C#/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Problema_1000___C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
