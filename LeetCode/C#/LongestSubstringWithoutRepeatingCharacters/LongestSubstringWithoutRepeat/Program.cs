8b27652f6925276ffbe54513a9d1492a4c90873e
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-03-13 22:07:36 -0300

    LongestStringWithoutRepeat

---
+++ LeetCode/C#/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeat/Program.cs
@@ -0,0 +1,24 @@
+﻿string s = "pwwkew";
+
+int result = LengthOfLongestSubstring(s);
+Console.WriteLine(result);
+
+int LengthOfLongestSubstring(string s)
+{
+    int maxLength = 0;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        HashSet<char> charSet = new HashSet<char>();
+        for (int j = i; j < s.Length; j++)
+        {
+            if (charSet.Contains(s[j]))
+            {
+                break;
+            }
+            charSet.Add(s[j]);
+            maxLength = Math.Max(maxLength, charSet.Count);
+        }
+    }
+    return maxLength;
+}
