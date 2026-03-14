e9502387603ad9e21b10b4c2a21a44a27700111a
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-03-13 21:56:38 -0300

    Desafio Add Two Number Leet Code

---
+++ LeetCode/C#/AddTwoNumbers/AddTwoNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿ListNode l1 = new(2, new(4, new(3)));
+ListNode l2 = new(5, new(6, new(4)));
+ListNode result = AddTwoNumbers(l1, l2);
+Console.WriteLine(result.val);
+Console.WriteLine(result.next.val);
+Console.WriteLine(result.next.next.val);
+
+
+ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+{
+    ListNode dummyHead = new(0);
+    ListNode current = dummyHead;
+    int carry = 0;
+
+    while (l1 != null || l2 != null || carry != 0)
+    {
+        int x = l1 != null ? l1.val : 0;
+        int y = l2 != null ? l2.val : 0;
+        int sum = x + y + carry;
+        carry = sum / 10;
+        current.next = new(sum % 10);
+        current = current.next;
+        if (l1 != null) l1 = l1.next;
+        if (l2 != null) l2 = l2.next;
+    }
+
+    return dummyHead.next;
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode? next;
+
+    public ListNode(int val = 0, ListNode? next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
